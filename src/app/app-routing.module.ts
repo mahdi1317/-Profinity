c547fa9c9bcb004978202c09848298ae03866550
mahdi <mejri4714@gmail.com>
2023-12-17 11:26:13 +0100

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './layout/home/home.component';
+import { DemoComponent } from './layout/demo/demo.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { JobsComponent } from './layout/jobs/jobs.component';
+import { PricingComponent } from './layout/pricing/pricing.component';
+import { SigninComponent } from './layout/signin/signin.component';
+import { SignupComponent } from './layout/signup/signup.component';
+import { SolutionsComponent } from './layout/solutions/solutions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  {
+    component:HomeComponent,
+    path:'home'
+   },
+   {
+    component:DemoComponent,
+    path:'demo'
+   },
+   {
+    component:JobsComponent,
+    path:'jobs'
+   },
+   {
+    component:PricingComponent,
+    path:'pricing'
+   },
+   {
+    component:SigninComponent,
+    path:'signin'
+   },
+   {
+    component:SignupComponent,
+    path:'signup'
+   },
+   {
+    component:SolutionsComponent,
+    path:'solutions'
+   },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './layout/home/home.component';
+import { SolutionsComponent } from './layout/solutions/solutions.component';
+import { PricingComponent } from './layout/pricing/pricing.component';
+import { JobsComponent } from './layout/jobs/jobs.component';
+
+import { SigninComponent } from './layout/signin/signin.component';
+import { DemoComponent } from './layout/demo/demo.component';
+import { SignupComponent } from './layout/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    SolutionsComponent,
+    PricingComponent,
+    JobsComponent,
+    
+    SigninComponent,
+    DemoComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
